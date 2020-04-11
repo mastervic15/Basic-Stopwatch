f7fc5b6f270cc4eb7468a4a8f8ab3b1ef042e586
mastervic15 <61251610+mastervic15@users.noreply.github.com>
2020-04-11 18:45:32 -0500

    return this .state to comfirm if watch work. but still having problems with display

--- stopwatch/src/Components/Watch.js
+++ stopwatch/src/Components/Watch.js
@@ -8,13 +8,19 @@ class Watch extends React.Component {
         this.state = {minute:0, second: 0};
     }
     componentDidMount(){
-        setInterval(function){
+        setInterval(function()){
+            return this.setState((state, props)=>{
             
-        }
+            return{
+                second: state.second ==59?0:state.second+1
+                minute:state.second==59?state.minute+1:state.minute
+            }
+            });
+        }, 1000)
     }
     render() {
         return(
-            <p>Stop Watch</p>
+        <p>{this.state.minute}:{this.state.second}</p>
         );
     }
 }
