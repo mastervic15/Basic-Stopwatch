c4b8c01a0899da611810d4514f96e93f75866d8e
mastervic15 <61251610+mastervic15@users.noreply.github.com>
2020-04-11 18:34:09 -0500

    Using componentDidMount and setInterval

--- stopwatch/src/Components/Watch.js
+++ stopwatch/src/Components/Watch.js
@@ -1,11 +1,21 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 
 
 class Watch extends React.Component {
     constructor(){
         super();
         this.state = {minute:0, second: 0};
+    }
+    componentDidMount(){
+        setInterval(function){
+            
+        }
+    }
+    render() {
+        return(
+            <p>Stop Watch</p>
+        );
     }
 }
 
