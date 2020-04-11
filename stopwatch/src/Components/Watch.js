dce688abc56c42be35edf76676d055e3acc958ba
mastervic15 <61251610+mastervic15@users.noreply.github.com>
2020-04-11 18:27:59 -0500

    Using this.state

---
+++ stopwatch/src/Components/Watch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+
+class Watch extends React.Component {
+    constructor(){
+        super();
+        this.state = {minute:0, second: 0};
+    }
+}
+
+export default Watch;
